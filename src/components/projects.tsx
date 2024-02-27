74a7909a761b1f4f8593731ed134e8ca8379b7e7
BBennettChiba <brysoncbennett@gmail.com>
2024-02-27 20:19:38 +0900

    removed 2 projects and their images

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -12,15 +12,9 @@ import dynamodb from "/public/dynamodb.svg";
 import graphql from "/public/graphql.png";
 import node from "/public/node.png";
 import reactnative from "/public/reactnative.png";
-import garbagemonster from "/public/garbage monster.png";
-import heroku from "/public/heroku.png";
-import mymizu from "/public/mymizu.png";
 import postgres from "/public/postgres.png";
-import typeorm from "/public/typeorm.png";
 import vercel from "/public/vercel.png";
 import react from "/public/react.png";
-import vue from "/public/vue.png";
-import truckstop from "/public/truckStop.png";
 import bookDetails from "/public/Book Details.jpg";
 import ownedBooks from "/public/Owned Books.jpg";
 import toRead from "/public/To Read.jpg";
@@ -127,20 +121,6 @@ const YHDes = {
 On the frontend I wrote the matching logic so users can find one another based on what books they enjoy, and the chat functionality implementing Appsync technology like graphql subscriptions which allowed matched users to chat with one another.`,
   publicDemo: "https://www.youtube.com/watch?v=S9GBXQEosK4&t=534s",
 };
-const BMDes = {
-  title: "Position: Full Stack Engineer",
-  data: `A React web app created in collaboration with myMizu which gamified water drinking by turning 
-it into an RPG monster battler in which users refill their water using the myMizu API and grow stronger. It was created by a multicultural team over 
-3 days. My Mizu is a NPO that is trying to reduce plastic from water bottles worldwide. They shared their API and we were charged with making an 
-application around that.`,
-  publicDemo: "https://youtu.be/vwMpFxsuQgQ?t=2042",
-};
-const GRDes = {
-  title: "Position: Full Stack Engineer",
-  data: `A Vue web app practice project for truckers. It too was created over 3 days by a 
-multicultural team. It was my first full-stack group project. Truckers can search and filter using the app for a truckstop nearby. I wrote the 
-entire backend myself in Typescript using TypeORM and an express server. I'm pretty proud of that.`,
-};
 const RNBRDes = {
   title: "Full Stack Solo project",
   data: `A React Native app for scanning your books for recordings purposes and adding books to a to be read list. My first React Native solo project. 
@@ -197,23 +177,6 @@ const projects = [
       node,
       reactnative,
     ],
-  },
-  {
-    name: "BlasMisu",
-    description: description(BMDes),
-    video: null,
-    front: "https://github.com/Shinji-Nishikita/team-project-mymizu-front",
-    back: "https://github.com/Shinji-Nishikita/team-project-mymizu-back",
-    img: [garbagemonster],
-    tech: [heroku, mymizu, node, postgres, react, typeorm, vercel],
-  },
-  {
-    name: "Go React",
-    description: description(GRDes),
-    video: null,
-    front: "https://github.com/BBennettChiba/go-react",
-    img: [truckstop],
-    tech: [vue, node, heroku, typeorm, postgres],
   },
   {
     name: "React Native Book Records",
